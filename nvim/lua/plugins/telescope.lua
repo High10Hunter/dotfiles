6ee0eb4e6b0bff425993c1e809c95ebd21859cd1
High10Hunter <trandaiquy2903@gmail.com>
2024-12-22 17:36:45 +0700

    update telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,12 +3,23 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files Telescope" },
+      {
+        "<leader>fh",
+        "<cmd>Telescope live_grep vimgrep_arguments=rg,--color=never,--no-heading,--no-heading,--line-number,--column,--smart-case,--hidden,--no-ignore<cr>",
+        desc = "Find in hidden files",
+      },
     },
     opts = {
       pickers = {
         find_files = {
-          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          hidden = true,
           find_command = { "rg", "--files", "--glob", "!**/.git/*", "-L" },
+        },
+        grep_string = {
+          additional_args = { "--hidden", "--smart-case" },
+        },
+        live_grep = {
+          additional_args = { "--hidden", "--smart-case" },
         },
       },
     },
@@ -26,17 +37,4 @@ return {
   {
     "nvim-telescope/telescope-symbols.nvim",
   },
-
-  -- Custom ripgrep configuration:
-
-  -- I want to search in hidden/dot files.
-  -- "--hidden"
-  --
-  -- I don't want to search in the `.git` directory.
-  -- "--glob")
-  -- "!**/.git/*")
-  --
-  --  I want to follow symbolic links
-  -- "-L"
-  --
 }
